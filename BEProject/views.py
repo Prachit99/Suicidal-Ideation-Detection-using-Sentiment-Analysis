d537e0a462be9100f9dbdf32ba5c04b969ad10d1
rdoshi29 <riddhi.rd@somaiya.edu>
2021-03-20 11:50:43 +0530

    first commit

---
+++ BEProject/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+import joblib
+
+def login(request):
+    return render(request, "login.html")
+
+def test(request):
+    #cls = joblib.load("model.sav")
+    output = request.POST.get('text_post', False)
+    #    output = cls.predict([raw_text])
+    return render(request, "test.html", {'output': output})
+
